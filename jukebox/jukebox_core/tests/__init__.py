4c067a49a5133b68cd415fb1b9df596709c21922
Jens Nistler <opensource@jensnistler.de>
2011-11-20 17:48:51 +0100

    jukebox_core: added unittests for years api

--- jukebox/jukebox_core/tests/__init__.py
+++ jukebox/jukebox_core/tests/__init__.py
@@ -4,3 +4,4 @@ from .api_songs import *
 from .api_artists import *
 from .api_albums import *
 from .api_genres import *
+from .api_years import *
---
+++ jukebox/jukebox_core/tests/api_years.py
@@ -0,0 +1,121 @@
+# -*- coding: UTF-8 -*-
+
+import simplejson
+from jukebox_core.tests.api import ApiTestBase
+
+
+class ApiYearsTest(ApiTestBase):
+    def testIndexEmpty(self):
+        result = simplejson.loads(
+            self.httpGet(
+                "/api/v1/years"
+            ).content
+        )
+
+        self.assertEquals(len(result["itemList"]), 0)
+
+    def testIndex(self):
+        year = 2000
+        artist = self.addArtist()
+        self.addSong(artist, None, None, "TestTitle", year)
+
+        result = simplejson.loads(
+            self.httpGet(
+                "/api/v1/years"
+            ).content
+        )
+
+        self.assertEquals(len(result["itemList"]), 1)
+        self.assertEquals(result["itemList"][0]["year"], year)
+
+    def testIndexOrderBy(self):
+        year_a = 2000
+        year_b = 2010
+        artist = self.addArtist()
+        self.addSong(artist, None, None, "TestTitle", year_a)
+        self.addSong(artist, None, None, "TestTitle", year_b)
+
+        result = simplejson.loads(
+            self.httpGet(
+                "/api/v1/years?order_by=year"
+            ).content
+        )
+
+        self.assertEquals(len(result["itemList"]), 2)
+        self.assertEquals(result["itemList"][0]["year"], year_a)
+        self.assertEquals(result["itemList"][1]["year"], year_b)
+
+        result = simplejson.loads(
+            self.httpGet(
+                "/api/v1/years?order_by=year&order_direction=desc"
+            ).content
+        )
+
+        self.assertEquals(len(result["itemList"]), 2)
+        self.assertEquals(result["itemList"][0]["year"], year_b)
+        self.assertEquals(result["itemList"][1]["year"], year_a)
+
+    def testCount(self):
+        year_a = 2000
+        year_b = 2005
+        year_c = 2010
+        artist = self.addArtist()
+        self.addSong(artist, None, None, "TestTitle", year_a)
+        self.addSong(artist, None, None, "TestTitle", year_b)
+        self.addSong(artist, None, None, "TestTitle", year_c)
+
+        result = simplejson.loads(
+            self.httpGet(
+                "/api/v1/years?count=1"
+            ).content
+        )
+        self.assertEquals(len(result["itemList"]), 1)
+        self.assertEquals(result["itemList"][0]["year"], year_a)
+        self.assertTrue(result["hasNextPage"])
+
+        result = simplejson.loads(
+            self.httpGet(
+                "/api/v1/years?count=3"
+            ).content
+        )
+        self.assertEquals(len(result["itemList"]), 3)
+        self.assertEquals(result["itemList"][0]["year"], year_a)
+        self.assertEquals(result["itemList"][1]["year"], year_b)
+        self.assertEquals(result["itemList"][2]["year"], year_c)
+        self.assertFalse(result["hasNextPage"])
+
+    def testCountAndPage(self):
+        year_a = 2000
+        year_b = 2005
+        year_c = 2010
+        artist = self.addArtist()
+        self.addSong(artist, None, None, "TestTitle", year_a)
+        self.addSong(artist, None, None, "TestTitle", year_b)
+        self.addSong(artist, None, None, "TestTitle", year_c)
+
+        result = simplejson.loads(
+            self.httpGet(
+                "/api/v1/years?count=1&page=1"
+            ).content
+        )
+        self.assertEquals(len(result["itemList"]), 1)
+        self.assertEquals(result["itemList"][0]["year"], year_a)
+        self.assertTrue(result["hasNextPage"])
+
+        result = simplejson.loads(
+            self.httpGet(
+                "/api/v1/years?count=1&page=2"
+            ).content
+        )
+        self.assertEquals(len(result["itemList"]), 1)
+        self.assertEquals(result["itemList"][0]["year"], year_b)
+        self.assertTrue(result["hasNextPage"])
+
+        result = simplejson.loads(
+            self.httpGet(
+                "/api/v1/years?count=1&page=3"
+            ).content
+        )
+        self.assertEquals(len(result["itemList"]), 1)
+        self.assertEquals(result["itemList"][0]["year"], year_c)
+        self.assertFalse(result["hasNextPage"])
