b5f04c35d971d193ac1dc9308c711456dcef7912
Jens Nistler <opensource@jensnistler.de>
2011-11-20 17:21:16 +0100

    jukebox_core: changed unittests to test the actual rest api

--- jukebox/jukebox_core/tests/api.py
+++ jukebox/jukebox_core/tests/api.py
@@ -1,12 +1,30 @@
 # -*- coding: UTF-8 -*-
 
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.db import transaction
+import base64
 from jukebox_core.models import Artist, Album, Genre, Song
 from django.contrib.auth.models import User
-import jukebox_core.api
 
 
 class ApiTestBase(TestCase):
+    user = None
+    username = "TestUser"
+    email = "test@domain.org"
+    password = "TestPassword"
+
+    def setUp(self):
+        transaction.rollback()
+
+        # register test user and setup auth
+        self.user = self.addUser(self.username, self.email, self.password)
+
+    def httpGet(self, url, params={}):
+        auth = '%s:%s' % (self.username, self.password)
+        auth = "Basic %s" % base64.encodestring(auth).strip()
+        c = Client()
+        return c.get(url, params, HTTP_AUTHORIZATION=auth)
+
     def addArtist(self, name="TestArist"):
         artist = Artist(
             Name=name
@@ -52,10 +70,5 @@ class ApiTestBase(TestCase):
         song.save()
         return song
 
-    def addUser(self, username, password):
-        user = User(
-            username=username,
-            password=password
-        )
-        user.save()
-        return user
+    def addUser(self, username, email, password):
+        return User.objects.create_user(username, email, password)
--- jukebox/jukebox_core/tests/api_songs.py
+++ jukebox/jukebox_core/tests/api_songs.py
@@ -1,22 +1,20 @@
 # -*- coding: UTF-8 -*-
 
-from django.db import transaction
-from jukebox_core.models import Artist, Album, Genre, Song
-import random
-from jukebox_core.tests import api as apiBase
-import jukebox_core.api
+import random, simplejson
+from jukebox_core import api
+from jukebox_core.tests.api import ApiTestBase
 
 
-class ApiSongsTest(apiBase.ApiTestBase):
-    def setUp(self):
-        transaction.rollback()
-
+class ApiSongsTest(ApiTestBase):
     def testIndex(self):
         artist = self.addArtist()
         song = self.addSong(artist)
 
-        songs_api = jukebox_core.api.songs()
-        result = songs_api.index()
+        result = simplejson.loads(
+            self.httpGet(
+                "/api/v1/songs"
+            ).content
+        )
 
         self.assertEquals(len(result["itemList"]), 1)
         self.assertEquals(result["itemList"][0]["id"], song.id)
@@ -28,9 +26,11 @@ class ApiSongsTest(apiBase.ApiTestBase):
         artist = self.addArtist()
         song = self.addSong(artist, None, None, fixture)
 
-        songs_api = jukebox_core.api.songs()
-        songs_api.set_search_term(fixturePart)
-        result = songs_api.index()
+        result = simplejson.loads(
+            self.httpGet(
+                "/api/v1/songs?search_term=" + fixturePart
+            ).content
+        )
 
         self.assertEquals(len(result["itemList"]), 1)
         self.assertEquals(result["itemList"][0]["id"], song.id)
@@ -42,9 +42,11 @@ class ApiSongsTest(apiBase.ApiTestBase):
         artist = self.addArtist(fixture)
         song = self.addSong(artist)
 
-        songs_api = jukebox_core.api.songs()
-        songs_api.set_search_term(fixturePart)
-        result = songs_api.index()
+        result = simplejson.loads(
+            self.httpGet(
+                "/api/v1/songs?search_term=" + fixturePart
+            ).content
+        )
 
         self.assertEquals(len(result["itemList"]), 1)
         self.assertEquals(result["itemList"][0]["id"], song.id)
@@ -57,9 +59,11 @@ class ApiSongsTest(apiBase.ApiTestBase):
         album = self.addAlbum(artist, fixture)
         song = self.addSong(artist, album)
 
-        songs_api = jukebox_core.api.songs()
-        songs_api.set_search_term(fixturePart)
-        result = songs_api.index()
+        result = simplejson.loads(
+            self.httpGet(
+                "/api/v1/songs?search_term=" + fixturePart
+            ).content
+        )
 
         self.assertEquals(len(result["itemList"]), 1)
         self.assertEquals(result["itemList"][0]["id"], song.id)
@@ -71,9 +75,11 @@ class ApiSongsTest(apiBase.ApiTestBase):
         artist = self.addArtist()
         song = self.addSong(artist, None, None, fixture)
 
-        songs_api = jukebox_core.api.songs()
-        songs_api.set_search_title(fixturePart)
-        result = songs_api.index()
+        result = simplejson.loads(
+            self.httpGet(
+                "/api/v1/songs?search_title=" + fixturePart
+            ).content
+        )
 
         self.assertEquals(len(result["itemList"]), 1)
         self.assertEquals(result["itemList"][0]["id"], song.id)
@@ -85,9 +91,11 @@ class ApiSongsTest(apiBase.ApiTestBase):
         artist = self.addArtist(fixture)
         song = self.addSong(artist)
 
-        songs_api = jukebox_core.api.songs()
-        songs_api.set_search_artist_name(fixturePart)
-        result = songs_api.index()
+        result = simplejson.loads(
+            self.httpGet(
+                "/api/v1/songs?search_artist=" + fixturePart
+            ).content
+        )
 
         self.assertEquals(len(result["itemList"]), 1)
         self.assertEquals(result["itemList"][0]["id"], song.id)
@@ -100,9 +108,11 @@ class ApiSongsTest(apiBase.ApiTestBase):
         album = self.addAlbum(artist, fixture)
         song = self.addSong(artist, album)
 
-        songs_api = jukebox_core.api.songs()
-        songs_api.set_search_album_title(fixturePart)
-        result = songs_api.index()
+        result = simplejson.loads(
+            self.httpGet(
+                "/api/v1/songs?search_album=" + fixturePart
+            ).content
+        )
 
         self.assertEquals(len(result["itemList"]), 1)
         self.assertEquals(result["itemList"][0]["id"], song.id)
@@ -113,9 +123,11 @@ class ApiSongsTest(apiBase.ApiTestBase):
         artist = self.addArtist()
         song = self.addSong(artist, None, None, "TestTitle", fixture)
 
-        songs_api = jukebox_core.api.songs()
-        songs_api.set_filter_year(fixture)
-        result = songs_api.index()
+        result = simplejson.loads(
+            self.httpGet(
+                "/api/v1/songs?filter_year=" + str(fixture)
+            ).content
+        )
 
         self.assertEquals(len(result["itemList"]), 1)
         self.assertEquals(result["itemList"][0]["id"], song.id)
@@ -125,9 +137,11 @@ class ApiSongsTest(apiBase.ApiTestBase):
         genre = self.addGenre()
         song = self.addSong(artist, None, genre)
 
-        songs_api = jukebox_core.api.songs()
-        songs_api.set_filter_genre(genre.id)
-        result = songs_api.index()
+        result = simplejson.loads(
+            self.httpGet(
+                "/api/v1/songs?filter_genre=" + str(genre.id)
+            ).content
+        )
 
         self.assertEquals(len(result["itemList"]), 1)
         self.assertEquals(result["itemList"][0]["id"], song.id)
@@ -137,9 +151,11 @@ class ApiSongsTest(apiBase.ApiTestBase):
         album = self.addAlbum(artist)
         song = self.addSong(artist, album)
 
-        songs_api = jukebox_core.api.songs()
-        songs_api.set_filter_album_id(album.id)
-        result = songs_api.index()
+        result = simplejson.loads(
+            self.httpGet(
+                "/api/v1/songs?filter_album_id=" + str(album.id)
+            ).content
+        )
 
         self.assertEquals(len(result["itemList"]), 1)
         self.assertEquals(result["itemList"][0]["id"], song.id)
@@ -148,9 +164,11 @@ class ApiSongsTest(apiBase.ApiTestBase):
         artist = self.addArtist()
         song = self.addSong(artist)
 
-        songs_api = jukebox_core.api.songs()
-        songs_api.set_filter_artist_id(artist.id)
-        result = songs_api.index()
+        result = simplejson.loads(
+            self.httpGet(
+                "/api/v1/songs?filter_artist_id=" + str(artist.id)
+            ).content
+        )
 
         self.assertEquals(len(result["itemList"]), 1)
         self.assertEquals(result["itemList"][0]["id"], song.id)
@@ -162,16 +180,21 @@ class ApiSongsTest(apiBase.ApiTestBase):
         song_a = self.addSong(artist, album, genre, "A Title")
         song_b = self.addSong(artist, album, genre, "B Title")
 
-        songs_api = jukebox_core.api.songs()
-        songs_api.set_order_by("title", "asc")
-        result = songs_api.index()
+        result = simplejson.loads(
+            self.httpGet(
+                "/api/v1/songs?order_by=title"
+            ).content
+        )
 
         self.assertEquals(len(result["itemList"]), 2)
         self.assertEquals(result["itemList"][0]["id"], song_a.id)
         self.assertEquals(result["itemList"][1]["id"], song_b.id)
 
-        songs_api.set_order_by("title", "desc")
-        result = songs_api.index()
+        result = simplejson.loads(
+            self.httpGet(
+                "/api/v1/songs?order_by=title&order_direction=desc"
+            ).content
+        )
 
         self.assertEquals(len(result["itemList"]), 2)
         self.assertEquals(result["itemList"][0]["id"], song_b.id)
@@ -183,16 +206,21 @@ class ApiSongsTest(apiBase.ApiTestBase):
         song_a = self.addSong(artist_a)
         song_b = self.addSong(artist_b)
 
-        songs_api = jukebox_core.api.songs()
-        songs_api.set_order_by("artist", "asc")
-        result = songs_api.index()
+        result = simplejson.loads(
+            self.httpGet(
+                "/api/v1/songs?order_by=artist"
+            ).content
+        )
 
         self.assertEquals(len(result["itemList"]), 2)
         self.assertEquals(result["itemList"][0]["id"], song_a.id)
         self.assertEquals(result["itemList"][1]["id"], song_b.id)
 
-        songs_api.set_order_by("artist", "desc")
-        result = songs_api.index()
+        result = simplejson.loads(
+            self.httpGet(
+                "/api/v1/songs?order_by=artist&order_direction=desc"
+            ).content
+        )
 
         self.assertEquals(len(result["itemList"]), 2)
         self.assertEquals(result["itemList"][0]["id"], song_b.id)
@@ -205,16 +233,21 @@ class ApiSongsTest(apiBase.ApiTestBase):
         song_a = self.addSong(artist, album_a)
         song_b = self.addSong(artist, album_b)
 
-        songs_api = jukebox_core.api.songs()
-        songs_api.set_order_by("album", "asc")
-        result = songs_api.index()
+        result = simplejson.loads(
+            self.httpGet(
+                "/api/v1/songs?order_by=album"
+            ).content
+        )
 
         self.assertEquals(len(result["itemList"]), 2)
         self.assertEquals(result["itemList"][0]["id"], song_a.id)
         self.assertEquals(result["itemList"][1]["id"], song_b.id)
 
-        songs_api.set_order_by("album", "desc")
-        result = songs_api.index()
+        result = simplejson.loads(
+            self.httpGet(
+                "/api/v1/songs?order_by=album&order_direction=desc"
+            ).content
+        )
 
         self.assertEquals(len(result["itemList"]), 2)
         self.assertEquals(result["itemList"][0]["id"], song_b.id)
@@ -225,16 +258,21 @@ class ApiSongsTest(apiBase.ApiTestBase):
         song_a = self.addSong(artist, None, None, "TestTitle", 2000)
         song_b = self.addSong(artist, None, None, "TestTitle", 2001)
 
-        songs_api = jukebox_core.api.songs()
-        songs_api.set_order_by("year", "asc")
-        result = songs_api.index()
+        result = simplejson.loads(
+            self.httpGet(
+                "/api/v1/songs?order_by=year"
+            ).content
+        )
 
         self.assertEquals(len(result["itemList"]), 2)
         self.assertEquals(result["itemList"][0]["id"], song_a.id)
         self.assertEquals(result["itemList"][1]["id"], song_b.id)
 
-        songs_api.set_order_by("year", "desc")
-        result = songs_api.index()
+        result = simplejson.loads(
+            self.httpGet(
+                "/api/v1/songs?order_by=year&order_direction=desc"
+            ).content
+        )
 
         self.assertEquals(len(result["itemList"]), 2)
         self.assertEquals(result["itemList"][0]["id"], song_b.id)
@@ -247,16 +285,21 @@ class ApiSongsTest(apiBase.ApiTestBase):
         song_a = self.addSong(artist, None, genre_a)
         song_b = self.addSong(artist, None, genre_b)
 
-        songs_api = jukebox_core.api.songs()
-        songs_api.set_order_by("genre", "asc")
-        result = songs_api.index()
+        result = simplejson.loads(
+            self.httpGet(
+                "/api/v1/songs?order_by=genre"
+            ).content
+        )
 
         self.assertEquals(len(result["itemList"]), 2)
         self.assertEquals(result["itemList"][0]["id"], song_a.id)
         self.assertEquals(result["itemList"][1]["id"], song_b.id)
 
-        songs_api.set_order_by("genre", "desc")
-        result = songs_api.index()
+        result = simplejson.loads(
+            self.httpGet(
+                "/api/v1/songs?order_by=genre&order_direction=desc"
+            ).content
+        )
 
         self.assertEquals(len(result["itemList"]), 2)
         self.assertEquals(result["itemList"][0]["id"], song_b.id)
@@ -267,16 +310,21 @@ class ApiSongsTest(apiBase.ApiTestBase):
         song_a = self.addSong(artist, None, None, "TestTitle", 2000, 100)
         song_b = self.addSong(artist, None, None, "TestTitle", 2000, 200)
 
-        songs_api = jukebox_core.api.songs()
-        songs_api.set_order_by("length", "asc")
-        result = songs_api.index()
+        result = simplejson.loads(
+            self.httpGet(
+                "/api/v1/songs?order_by=length"
+            ).content
+        )
 
         self.assertEquals(len(result["itemList"]), 2)
         self.assertEquals(result["itemList"][0]["id"], song_a.id)
         self.assertEquals(result["itemList"][1]["id"], song_b.id)
 
-        songs_api.set_order_by("length", "desc")
-        result = songs_api.index()
+        result = simplejson.loads(
+            self.httpGet(
+                "/api/v1/songs?order_by=length&order_direction=desc"
+            ).content
+        )
 
         self.assertEquals(len(result["itemList"]), 2)
         self.assertEquals(result["itemList"][0]["id"], song_b.id)
@@ -286,13 +334,13 @@ class ApiSongsTest(apiBase.ApiTestBase):
         artist = self.addArtist()
         song = self.addSong(artist, None, None, "TestTitle", 2000, 100, __file__)
 
-        songs_api = jukebox_core.api.songs()
+        songs_api = api.songs()
         result = songs_api.getNextSong()
 
         self.assertEquals(result, song)
 
         # check if song has been added to history
-        history_api = jukebox_core.api.history()
+        history_api = api.history()
         result = history_api.index()
 
         self.assertEqual(result["itemList"][0]["id"], song.id)
@@ -301,21 +349,18 @@ class ApiSongsTest(apiBase.ApiTestBase):
         artist = self.addArtist()
         song = self.addSong(artist, None, None, "TestTitle", 2000, 100, __file__)
 
-        # add user
-        user = self.addUser("testUser", "testPassword")
-
         # add to queue
-        queue_api = jukebox_core.api.queue()
-        queue_api.set_user_id(user.id)
+        queue_api = api.queue()
+        queue_api.set_user_id(self.user.id)
         queue_api.add(song.id)
 
         # get next song
-        songs_api = jukebox_core.api.songs()
+        songs_api = api.songs()
         result = songs_api.getNextSong()
         self.assertEquals(result, song)
 
         # check if song has been added to history
-        history_api = jukebox_core.api.history()
+        history_api = api.history()
         result = history_api.index()
         self.assertEqual(result["itemList"][0]["id"], song.id)
 
